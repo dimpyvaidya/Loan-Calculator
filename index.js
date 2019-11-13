79aad59cc8557998e654cb3a17635c7f91cb7f2e
Dimpy <dimpy13parekh@gmail.com>
2019-11-12 19:44:01 -0500

    some calculation done

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-let btnCalculate = document.getElementById("loanTerm");
+let calc = document.getElementById("calc");
 
-function calLoanTerm() {
-    let years = document.getElementById("years").value;
-    let result = parseFloat(years.value) * 12;
 
-    alert(`Number of payments required for ${years} is ${result}`);
+function calculate() {
+    let amount = document.getElementById("amount");
+    let apr = document.getElementById("apr");
+    let years = document.getElementById("years");
+
+    document.getElementById("payment").style.color = "red";
+    let total = document.getElementById("total");
+    let totalinterest = document.getElementById("totalinterest");
+
+    let principal = parseFloat(amount.value);
+    let interest = (parseFloat(apr.value) / 100) / 12;
+    let payments = parseFloat(years.value) * 12;
+
+    let x = Math.pow(1 + interest, payments);
+    let monthly = (principal * x * interest) / (x - 1);
+
+    amount.innerHTML = `$amount`;
+    payment.innerHTML = monthly.toFixed(2);
+    total.innerHTML = (monthly * payments).toFixed(2);
+    totalinterest.innerHTML = ((monthly * payments) - principal).toFixed(2);
+    return false;
+
 }
-//make the buttons eventlistener
-btnArea.addEventListener("click", calLoanTerm);
